2296b6a9d8009e51b3109b65db6b125da6d5e9b8
tekkahammadxo <107896001+tekkahammadxo@users.noreply.github.com>
2022-11-09 00:49:51 +0200

    Update HttpRequestHandler.php

--- src/HttpRequestHandler.php
+++ src/HttpRequestHandler.php
@@ -7,76 +7,27 @@ class HttpRequestHandler
     /**
      * @var string $apiUrl
      */
+    private $apiUrl = "https://app.fawaterk.com/api/invoice";
+
     /**
      * Send the request to the API
      */
-    public function getPaymentOptions( array $data )
+    public function send( array $data )
     {
-                $curl = curl_init();
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://fawaterkstage.com/api/v2/getPaymentmethods',
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'GET',
-          CURLOPT_HTTPHEADER => array(
+        $data = json_encode($data);
+        
+        $ch = curl_init($this->apiUrl);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
-            'Authorization: Bearer d83a5d07aaeb8442dcbe259e6dae80a3f2e21a3a581e1a5acd'
-          ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-    }
-    public function initPayment( array $data )
-    {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://fawaterkstage.com/api/v2/invoiceInitPay',
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS =>'{
-            "payment_method_id": 2,
-            "cartTotal": "50",
-            "currency": "EGP",
-            "customer": {
-                "first_name": "mohammad",
-                "last_name": "hamza",
-                "email": "test@fawaterk.com",
-                "phone": "01xxxxxxxxx",
-                "address": "test address"
-            },
-            "redirectionUrls": {
-                 "successUrl" : "https://dev.fawaterk.com/success",
-                 "failUrl": "https://dev.fawaterk.com/fail",
-                 "pendingUrl": "https://dev.fawaterk.com/pending"   
-            },
-            "cartItems": [
-                {
-                    "name": "this is test oop 112252",
-                    "price": "25",
-                    "quantity": "1"
-                },
-                {
-                    "name": "this is test oop 112252",
-                    "price": "25",
-                    "quantity": "1"
-                }
-            ]
-        }',
-          CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json',
-            'Authorization: Bearer d83a5d07aaeb8442dcbe259e6dae80a3f2e21a3a581e1a5acd'
-          ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+            'Content-Length: ' . strlen($data))
+        );
+
+        $curlResult = curl_exec($ch);
+
+        return json_decode($curlResult);
     }
 }
